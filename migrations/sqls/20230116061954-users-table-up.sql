5ed3d48892ae8be1028d4b478fb8a4aa82435f1d
hanin77 <mohammadhanin77@gmail.com>
2023-01-16 19:18:56 +0000

    feat: change passsword length to 255

--- migrations/sqls/20230116061954-users-table-up.sql
+++ migrations/sqls/20230116061954-users-table-up.sql
@@ -2,4 +2,4 @@ CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY  KEY,
     firstname VARCHAR(100),
     lastname VARCHAR(100),
     username VARCHAR(100) UNIQUE,
-    password VARCHAR(50));
+    password VARCHAR(255));
